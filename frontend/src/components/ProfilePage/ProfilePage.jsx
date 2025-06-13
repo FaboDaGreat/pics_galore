d4875f5e2c9f6110aefb4a33c8b4a67dd7c873e2
FaboDaGreat <fabian.richmond92@gmail.com>
2025-06-13 02:52:40 -0400

    Added message and another upload button that appears if a user has does not have any posts

--- frontend/src/components/ProfilePage/ProfilePage.jsx
+++ frontend/src/components/ProfilePage/ProfilePage.jsx
@@ -9,7 +9,7 @@ const ProfilePage = () => {
     const photos = useSelector((state) => state.photosReducer.allPhotos);
     const sortedPhotos = photos.sort((a, b) => b.id - a.id);
     const user = useSelector((state) => state.session.user);
-    
+
     useEffect(() => {
     
         const getMyPhotos = async () => {
@@ -25,23 +25,32 @@ const ProfilePage = () => {
     
       if (!isLoaded) {
         return <h1>Loading...</h1>
-      } else {
+    } 
+
+    else {
         return (
+    <div>
+      <h1>My Profile!</h1>
+      {sortedPhotos.length === 0 ? (
           <div>
-            <h1>My Profile!</h1>
-            <div>
-              {
-                sortedPhotos.map((photo, idx) => (
-                  <div key={`${idx}-${photo.id}`}>
-                    <img src={photo.url} />
-                  </div>
-                ))
-              }
-            </div>
+            <h2>You have no posts.</h2>
+            <button className="upload-photo-button">
+              Upload Photo
+            </button>
           </div>
-    
-        )
-      }
+        ) : (
+          <div>
+            {sortedPhotos.map((photo, idx) => (
+              <div key={`${idx}-${photo.id}`}>
+                <img src={photo.url} />
+              </div>
+            ))}
+          </div>
+        )}
+    </div>
+  );
 }
+      }
+
 
 export default ProfilePage
